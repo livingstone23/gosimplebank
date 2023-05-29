8299dabaa46baf8c249b0ce759eb12ecf768003c
livingstone23 <livingstone23@gmail.com>
2023-05-29 20:12:06 +0200

    Load config from file & environment variables in Go with Viper

--- db/sqlc/main_test.go
+++ db/sqlc/main_test.go
@@ -3,25 +3,31 @@ package db
 import (
 	"database/sql"
 	_ "github.com/lib/pq"
+	"github.com/livingstone23/gosimplebank/util"
 	"log"
 	"os"
 	"testing"
 )
 
+/*
 const (
 	dbDriver = "postgres"
 	dbSource = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 )
+*/
 
 var testQueries *Queries
 var testDB *sql.DB
 
-//TestMain, take testing.M object as input
-//is the main entry point
+// TestMain, take testing.M object as input
+// is the main entry point
 func TestMain(m *testing.M) {
-	var err error
+	config, err := util.LoadConfig("../..")
+	if err != nil {
+		log.Fatal("Cannot load config: ", err)
+	}
 
-	testDB, err = sql.Open(dbDriver, dbSource)
+	testDB, err = sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err.Error())
 	}
--- main.go
+++ main.go
@@ -6,19 +6,26 @@ import (
 	_ "github.com/lib/pq"
 	"github.com/livingstone23/gosimplebank/api"
 	db "github.com/livingstone23/gosimplebank/db/sqlc"
+	"github.com/livingstone23/gosimplebank/util"
 	"log"
 )
 
+/*
 const (
 	dbDriver      = "postgres"
 	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
 	serverAddress = "0.0.0.0:8080"
 )
+*/
 
 func main() {
+	config, err := util.LoadConfig(".")
+	if err != nil {
+		log.Fatal("cannot load config: ", err)
+	}
 	fmt.Println("Start SimpleBank")
 
-	conn, err := sql.Open(dbDriver, dbSource)
+	conn, err := sql.Open(config.DBDriver, config.DBSource)
 	if err != nil {
 		log.Fatal("cannot connect to db:", err.Error())
 	}
@@ -26,7 +33,7 @@ func main() {
 	store := db.NewStore(conn)
 	server := api.NewServer(store)
 
-	err = server.Start(serverAddress)
+	err = server.Start(config.ServerAddress)
 	if err != nil {
 		log.Fatal("cannot start server", err)
 	}
