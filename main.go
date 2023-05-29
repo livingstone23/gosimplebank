8f8975d49e1bbace907ff5854c2da1010a0caeed
livingstone23 <livingstone23@gmail.com>
2023-05-29 19:07:01 +0200

    Implementing Restful HTTP API in Go using Gin

--- main.go
+++ main.go
@@ -1,8 +1,34 @@
 package main
 
-import "fmt"
+import (
+	"database/sql"
+	"fmt"
+	_ "github.com/lib/pq"
+	"github.com/livingstone23/gosimplebank/api"
+	db "github.com/livingstone23/gosimplebank/db/sqlc"
+	"log"
+)
+
+const (
+	dbDriver      = "postgres"
+	dbSource      = "postgresql://root:secret@localhost:5432/simple_bank?sslmode=disable"
+	serverAddress = "0.0.0.0:8080"
+)
 
 func main() {
 	fmt.Println("Start SimpleBank")
+
+	conn, err := sql.Open(dbDriver, dbSource)
+	if err != nil {
+		log.Fatal("cannot connect to db:", err.Error())
+	}
+
+	store := db.NewStore(conn)
+	server := api.NewServer(store)
+
+	err = server.Start(serverAddress)
+	if err != nil {
+		log.Fatal("cannot start server", err)
+	}
 
 }
