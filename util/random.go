10e0e7b2314e17c04a900d929f28513e3939a099
livingstone23 <livingstone23@gmail.com>
2023-05-30 20:32:23 +0200

    Implement transfer money API with a custom params validator

--- util/random.go
+++ util/random.go
@@ -42,7 +42,7 @@ func RandomMoney() int64 {
 
 // RandomCurrency generate a random owner name
 func RandomCurrency() string {
-	currencies := []string{"EUR"}
+	currencies := []string{EUR, USD, CAD}
 	n := len(currencies)
 	return currencies[rand.Intn(n)]
 }
